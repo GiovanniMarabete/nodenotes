caf94a2b9ac4254f6c1d45fe089f12cfddbc6462
Giovanni Marabete <gmarabete@gmail.com>
2017-03-08 23:00:48 +0000

    First commit

---
+++ notes.js
@@ -0,0 +1,57 @@
+console.log('Starting notes.js');
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+    var notesString = fs.readFileSync('notes-data,json');
+    return JSON.parse(notesString);
+    } catch (e) {
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data,json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  // console.log('Adding note', title, body);
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+
+
+// var duplicateNotes = notes.filter((note) => {
+//   return note.title === title;
+
+// });
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
